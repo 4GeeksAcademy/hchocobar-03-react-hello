f4f9165a8a1a8decf90f1c64e8da257f2dc54a6e
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-04-17 12:40:42 -0300

    Update desestructuracion.js

--- src/js-examples/desestructuracion.js
+++ src/js-examples/desestructuracion.js
@@ -1,3 +1,4 @@
+/* Desestructuración de un array */
 const foo = [1, 3, 5 , 7 , 9]
 
 console.log(foo[0])
@@ -7,3 +8,8 @@ console.log(foo[0])
 [bar, buz] = foo
 console.log(bar)  // bar toma el valor del **primer** elemento de foo
 console.log(buz)  // buz toma el valor del **segundo** elemento de foo
+
+/* Desestructuración de un objeto */
+const user = {id: 18, name: 'Hector', lastname: 'chocobar'} 
+const { id, lastname } = user
+console.log(id)
