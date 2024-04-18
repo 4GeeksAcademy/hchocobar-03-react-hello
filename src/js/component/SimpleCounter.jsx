202e4f965078d70818cce1ddaa9555ec51532d77
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2024-04-18 03:53:34 +0000

    feat: SimpleCounterBonus ready; wip: SimpleCounter.jsx

---
+++ src/js/component/SimpleCounter.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+
+export const SimpleCounter = () => {
+  const [counter, setCounter] = useState(0);
+  const [isRunning, setIsRunning] = useState(false)
+  const [textStart, setTextStart] = useState('Start');
+  const [title, setTitle] = useState('Clock')
+  const [icon, setIcon] = useState("far fa-clock")
+
+  const handleStart = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      setTextStart('Stop')
+      setTitle('Chronometer')
+      setIcon("fas fa-stopwatch")
+      const newInterval = setInterval(() => {
+        setCounter((counter) => { return counter + 1 })
+      }, 10)
+    } else {
+      // clearInterval(newInterval)
+      setIsRunning(false)
+      setTextStart('Start')
+    }
+  }
+
+
+
+  const interval = () => {
+
+  }
+
+  return (
+    <div className="container">
+      <h1>Simple Counter</h1>
+      <h2 className={"text-primary"}>{title}</h2>
+      <div className="big-counter">
+        <div><i className={icon}></i></div>
+        <div>{Math.floor(counter / 100000000 % 10)}</div>
+        <div>{Math.floor(counter / 10000000 % 10)}</div>
+        <div>{Math.floor(counter / 1000000 % 10)}</div>
+        <div>{Math.floor(counter / 100000 % 10)}</div>
+        <div>{Math.floor(counter / 10000 % 10)}</div>
+        <div>{Math.floor(counter / 1000 % 10)}</div>
+        <div>{Math.floor(counter / 100 % 10)}</div>
+        <div>{','}</div>
+        <div>{Math.floor(counter / 10 % 10)}</div>
+        <div>{Math.floor(counter % 10)}</div>
+        <div className="btn-group-vertical" role="group" aria-label="Vertical button group">
+
+          <button onClick={handleStart} type="button" className="btn btn-outline-success">
+            {textStart}
+          </button>
+          <button type="button" className="btn btn-outline-danger">
+            Reset
+          </button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/js/component/SimpleCounterBonus.jsx
+++ src/js/component/SimpleCounterBonus.jsx
@@ -1,62 +1,106 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 export const SimpleCounterBonus = () => {
-  const [counter, setCounter] = useState(0);
-  const [isRunning, setIsRunning ] = useState(false)
-  const [textStart, setTextStart] = useState('Start');
-  const [title, setTitle] = useState('Clock')
-  const [icon, setIcon] = useState("far fa-clock")
+	const [counter, setCounter] = useState(0);
+	const [isRunning, setIsRunning] = useState(false);
+    const [status, setStatus] = useState({icon: "fas fa-clock", 
+                                          title: "Clock", 
+                                          titleStyle:"text-center text-success"});
+	const [countUp, setCountUp] = useState(true);
 
-  const handleStart = () => {
-    if (!isRunning) {
-      setIsRunning(true);
-      setTextStart('Stop')
-      setTitle('Chronometer')
-      setIcon("fas fa-stopwatch")
-      const newInterval = setInterval(() => {
-        setCounter( (counter) => { return counter + 1 } )
-      }, 10)
-    } else {
-      // clearInterval(newInterval)
-      setIsRunning(false)
-      setTextStart('Start')
+    const handleStart = () => {
+        setIsRunning(!isRunning);
+        if (countUp) {
+            setStatus({icon: "fas fa-stopwatch", 
+                       title:"Chronometer", 
+                       titleStyle:"text-center text-primary"});
+        } else {
+            setStatus({icon: "fas fa-history", 
+                       title:"Timer", 
+                       titleStyle:"text-center text-danger"});
+        }
+    };
+
+	const handleReset = () => {
+		setCounter(0);
+		setIsRunning(false);
+        setStatus({icon:"fas fa-clock", 
+                   title:"Clock", 
+                   titleStyle:"text-center text-success"});
+        setCountUp(true);
+	};
+
+    const handleTimer = (event) => {
+        if (event.target.value !== null && 
+            event.target.value >= 0 && 
+            event.target.value.length > 0 && 
+            !isNaN(event.target.value)) 
+        {
+            setCounter(parseInt(event.target.value));
+            setStatus({icon: "fas fa-history", 
+                       title:"Timer", 
+                       titleStyle:"text-center text-danger"});
+            setCountUp(false);
+        } else {
+            setCounter(0);
+            event.target.value = "";
+        }
     }
-  }
 
-  
+	useEffect(() => {
+		if (isRunning) {
+			const newInterval = setInterval(() => {
+				if (countUp) {
+					setCounter(counter => counter + 1);
+				} else {
+                    if (countUp === false && counter >= 0) {
+                        if (counter === 0) {
+                            setCounter(0);
+                            setIsRunning(false);
+                        } else {
+                            setCounter(counter => counter - 1);
+                        }
+    				}
+                }
+			}, 10);
 
-  const interval = () => {
+			return () => clearInterval(newInterval);
+		}
+	}, [isRunning, counter, countUp]);
 
-  }
 
-  return (
-    <div className="container">
-      <h1>Simple Counter</h1>
-      <h2 className={"text-primary"}>{title}</h2>
-      <div className="big-counter">
-        <div><i className={icon}></i></div>
-        <div>{Math.floor(counter / 100000000 % 10)}</div>
-        <div>{Math.floor(counter / 10000000 % 10)}</div>
-        <div>{Math.floor(counter / 1000000 % 10)}</div>
-        <div>{Math.floor(counter / 100000 % 10)}</div>
-        <div>{Math.floor(counter / 10000 % 10)}</div>
-        <div>{Math.floor(counter / 1000 % 10)}</div>
-        <div>{Math.floor(counter / 100 % 10)}</div>
-        <div>{','}</div>
-        <div>{Math.floor(counter / 10 % 10)}</div>
-        <div>{Math.floor(counter % 10)}</div>
-        <div>
-          <div className="btn-group-vertical" role="group" aria-label="Vertical button group">
-            <button onClick={handleStart} type="button" className="btn btn-outline-success">
-              {textStart}
-              </button>
-            <button type="button" className="btn btn-outline-danger">
-              Reset
-            </button>
-          </div>
+    return ( 
+        <div className="container">
+            <h1 className="mt-5">Simple Counter</h1>
+            <h2 className={status.titleStyle}>{status.title}</h2>
+            <div className="big-counter">
+                <div><i className={status.icon}/></div>
+                <div>{Math.floor(counter / 100000000) % 10}</div>
+                <div>{Math.floor(counter / 10000000) % 10}</div>
+                <div>{Math.floor(counter / 1000000) % 10}</div>
+                <div>{Math.floor(counter / 100000) % 10}</div>
+                <div>{Math.floor(counter / 10000) % 10}</div>
+                <div>{Math.floor(counter / 1000) % 10}</div>
+                <div>{Math.floor(counter / 100) % 10}</div>
+                <div>{','}</div>
+                <div>{Math.floor(counter / 10) % 10}</div>
+                <div>{Math.floor(counter) % 10}</div>
+                <div className="btn-group-vertical" role="group" aria-label="Vertical button group">
+                    <button type="button" onClick={handleStart} className="btn btn-outline-success">
+                        {isRunning ? 'Pause' : counter == 0 ? 'Start' : 'Continue'}
+                    </button>
+                    <button type="button" onClick={handleReset} className="btn btn-outline-danger">
+                        Reset
+                    </button>
+                </div>
+            </div>
+            <div className="container bg-dark">
+                <div className="input-group my-3 p-3">
+                    <span className="input-group-text bg-warning">Set Timer</span>
+                    <input type="text" onChange={(event) => handleTimer(event)} aria-label="First name" className="form-control" placeholder="Set the timer in tenths of a second"/>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
