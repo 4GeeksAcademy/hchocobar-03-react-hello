1b2ec81d1841a6edd29af66ac3b882ad2cc8abd8
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2024-04-29 16:07:18 +0000

    [navbar]fix: relocate buttonCallAction into ul

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -15,7 +15,6 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">{cohorte}</a>
-        <BtnReadMore/>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -24,6 +23,7 @@ function Navbar() {
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
+            <BtnReadMore/>
             <li className="nav-item">
               <a className="nav-link" href="#">Enlaces</a>
             </li>
