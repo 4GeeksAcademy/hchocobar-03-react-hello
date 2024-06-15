a205d6475d52ba32f79c115cb77e46d5a7655c4d
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-06-15 01:54:49 +0000

    sp72 - card wiht proptypes

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,20 +1,38 @@
 import React from "react";
-import image from '../../img/React1.png'
+import PropTypes from "prop-types";
+// import image from '../../img/React1.png';
+import logo from '../../img/logo-hcht-uk.jpg';
 
-export const Card = () => {
+
+export const Card = (props) => {
   // 3.  code js
 
     
   // 4.
   return (
-    <div className="card" style={{width: "25rem"}}>
+    <div className="card" style={{width: "18rem"}}>
       {/* style="width: 18rem;" */}
-      <img src={image} className="card-img-top" alt="..."/>
+      <img src={props.image} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.description}</p>
+          <a href="#" className="btn btn-primary">{props.textButton}</a>
         </div>
     </div>
   )
+}
+
+
+Card.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  textButton: PropTypes.string,
+  image: PropTypes.string
+}
+
+Card.defaultProps = {
+  title: 'Card Title',
+  description: 'Alguna descripción de tu contenido',
+  textButton: 'Leer más...',
+  image: logo
 }
