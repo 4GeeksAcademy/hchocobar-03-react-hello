6d22806bd7d53426266d65746adf9e7a5b21a507
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-11-10 16:19:21 +0000

    [sp-123] class6: initial commmit

---
+++ src/js/components/TodoList.jsx
@@ -0,0 +1,32 @@
+// 1.
+
+
+// 5. y 2.
+export const TodoList = () => {
+
+  return (
+
+    <div className="container my-3 text-start">
+      <div className="row">
+        <div className="col-10 col-sm-8 col-md-6 m-auto">
+          <h1 className="text-success text-center">Todo List</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-10 col-sm-8 col-md-6 m-auto">
+          <h2 className="text-primary">Add Task</h2>
+
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="row">
+        <div className="col-10 col-sm-8 col-md-6 m-auto">
+          <h2 className="text-primary">List</h2>
+
+        </div>
+      </div>
+    </div>
+  )
+}
